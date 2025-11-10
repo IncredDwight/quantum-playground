ee5e10e32a40e57b3258be41066fd232068ef33a
VYO <yaravirus86@gmail.com>
2025-11-09 19:10:35 -0500

    update

--- entanglement.py
+++ entanglement.py
@@ -8,16 +8,13 @@ qc.h(0)
 qc.cx(0, 1)
 qc.measure([0, 1], [0, 1])
 
-# 2. Set up the Aer simulator
+
 simulator = Aer.get_backend('aer_simulator')
 
-# 3. Transpile circuit for the simulator
 compiled_circuit = transpile(qc, simulator)
 
-# 4. Run simulation
 result = simulator.run(compiled_circuit, shots=1024).result()
 
-# 5. Extract and show results
 counts = result.get_counts()
 print(counts)
 plot_histogram(counts)
